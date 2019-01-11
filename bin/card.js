61e640a7af918799cd7f26427cb7fc90cc15b520
tien.nguyen <tien.nguyen@quoine.com>
2019-01-11 14:44:36 +0700

    Update version

--- bin/card.js
+++ bin/card.js
@@ -4,7 +4,7 @@ console.log('[32m[39m\n\
 [32m   ╭────────────────────────────────────────────────────────╮[39m\n\
 [32m   │                                                        │[39m\n\
 [32m   │   [37m           Nguyen Minh Tien[32m / [37mtiennguyen[32m             │[39m\n\
-[32m   │   [37m[1m   Title:[22m[32m  [37mFront-End Developer / Software Engineer[32m   │[39m\n\
+[32m   │   [37m[1m   Title:[22m[32m  [37mFront-End Developer[32m / [37mSoftware Engineer[32m   │[39m\n\
 [32m   │   [37m[1m    From:[22m[32m  [37mSai Gon, Vietnam[32m                          │[39m\n\
 [32m   │   [37m[1m  GitHub:[22m[32m  [90mhttps://github.com/[32m[32mtiennguyen1293[32m         │[39m\n\
 [32m   │   [37m[1m    Card:[22m[32m  [31mnpx[32m [37mtiennguyen[32m                            │[39m\n\
